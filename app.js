c324d86176eeeb8e26ce6e2fb0ee9ea10456ece0
joannemawodia <emawodia@gmail.com>
2025-10-06 20:15:37 -0300

    finalizando a estrutura do jogo

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 alert('Bem vindo ao jogo do número secreto');
 
-//armazenamento do número secreto
 let numeroSecreto = 2;
-let chute = prompt('Escolha um número entre 1 á 3');
+console.log(numeroSecreto);
+let chute;
 
-if (numeroSecreto == chute) {
-    alert('Isso mesmo, você descobriu o número secreto');
-} 
-if (numeroSecreto > chute){
-    alert('O número é mais alto')
+while ( chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 a 10');
+
+    if (numeroSecreto == chute) {
+        alert (`Isso mesmo, você descobriu o número secreto: ${numeroSecreto}`) ;
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O numero secreto é menor que ${chute}`)
+        } else {
+            alert(`O numero secreto é maior que ${chute}`)
+        }
+    }
 }
-if (numeroSecreto < chute){
-    alert('O número é mais baixo')
-}
+
+
 
