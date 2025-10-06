985b04c0adf430119fe1191de1c4e0ca04f6cc5c
joannemawodia <emawodia@gmail.com>
2025-10-06 11:06:05 -0300

    utilizando variaveis para armazenar dados do jogador

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 alert('Bem vindo ao jogo do número secreto');
+
+//armazenamento do número secreto
+let numeroSecreto = 2;
+let chute = prompt('Escolha um número entre 1 á 3');
+
+if (numeroSecreto == chute) {
+    alert('Isso mesmo, você descobriu o número secreto');
+} 
+if (numeroSecreto > chute){
+    alert('O número é mais alto')
+}
+if (numeroSecreto < chute){
+    alert('O número é mais baixo')
+}
+
