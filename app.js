d35a29ef8629aeb98f29339ea87b306e5fd6ca59
joannemawodia <emawodia@gmail.com>
2025-10-07 19:20:24 -0300

    acrescentando numero aleatorio

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 alert('Bem vindo ao jogo do número secreto');
 
-let numeroSecreto = 2;
-console.log(numeroSecreto);
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 let chute;
+let tentativas = 1;
 
 while ( chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 a 10');
+    chute = prompt('Escolha um número entre 1 a 100');
 
     if (numeroSecreto == chute) {
-        alert (`Isso mesmo, você descobriu o número secreto: ${numeroSecreto}`) ;
+        break;
     } else {
         if(chute > numeroSecreto){
-            alert(`O numero secreto é menor que ${chute}`)
+            alert(`O numero secreto é menor que ${chute}`);
         } else {
-            alert(`O numero secreto é maior que ${chute}`)
+            alert(`O numero secreto é maior que ${chute}`);
         }
+        tentativas++;
     }
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert (`Isso mesmo, você descobriu o número secreto: ${numeroSecreto}! Com ${tentativas} ${palavraTentativa}`);
 
 
